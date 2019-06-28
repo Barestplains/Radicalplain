548f8676c41b8682e9ede68a3f28f0e29cd3edfe
Barestplains <52296637+Barestplains@users.noreply.github.com>
2019-06-28 12:41:25 -0500

    Add files via upload

---
+++ Source1.cpp
@@ -0,0 +1,13 @@
+{
+"Category": "",
+"Description" : "",
+"EngineAssociation" : "4.22",
+"FileVersion" : 3,
+"Modules" : [
+{
+	"LoadingPhase": "Default",
+		"Name" : "Rapture",
+		"Type" : "Runtime"
+}
+]
+}
